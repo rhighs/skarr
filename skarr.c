fc7930c5f902c34324a4f9ecbae781e4170d4c4f
Roberto Montalti <rmontalti@technogym.com>
2023-03-10 09:45:41 +0100

    General improvement

--- skarr.c
+++ skarr.c
@@ -1,9 +1,19 @@
+// Author: 
+//     Roberto Montalti 2023
+//
+// File:
+//     skarr.c
+//
+
 #include "skarr.h"
 
 #include <stdlib.h>
 #include <memory.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
+
+#define DALLOC 4
 
 static
 skarr* __get_data(void* arr) {
@@ -16,13 +26,37 @@ u32 skarr_capacity(void** arr) { return __get_data(*arr)->cap; }
 static
 skarr* __skarr_realloc(skarr* m_data) {
     u32 new_cap = m_data->cap + m_data->cap/2;
+    if (new_cap == 0) new_cap = 4;
     m_data = realloc(m_data, sizeof(skarr) + new_cap);
     m_data->cap = new_cap;
     return m_data;
 }
 
+void skarr_free(void** arr) {
+    skarr* m_data = __get_data(*arr);
+    free(m_data);
+}
+
+void skarr_reset(void** arr) {
+    skarr* m_data = __get_data(*arr);
+    free(m_data->data);
+    m_data->cap = 0;
+    m_data->size = 0;
+}
+
+void skarr_clear(void** arr) {
+    skarr* m_data = __get_data(*arr);
+    m_data->size = 0;
+    memset(m_data->data, 0, m_data->cap);
+}
+
+void skarr_erase(void** arr) {
+    skarr* m_data = __get_data(*arr);
+    m_data->size = 0;
+}
+
 void* skarr_create(void** dst, u32 size, u32 n) {
-    u32 bufsize = size * (n != 0 ? n : 4);
+    u32 bufsize = size * (n != 0 ? n : DALLOC);
     skarr* out = (skarr*)malloc(sizeof(skarr) + bufsize);
     out->cap = bufsize;
     out->size = 0;
@@ -41,3 +75,9 @@ void* __skarr_push(void** arr, u32 typesize) {
     return (void*)&m_data->data[typesize * m_data->size++];
 }
 
+void* __skarr_pop(void** arr, u32 typesize) {
+    skarr* m_data = __get_data(*arr);
+    assert(m_data->size > 0);
+    u32 pos = (m_data->size--) - 1;
+    return (void*)&m_data->data[typesize * pos];
+}
--- skarr.h
+++ skarr.h
@@ -1,3 +1,9 @@
+// Author: 
+//     Roberto Montalti 2023
+//
+// File:
+//     skarr.h
+
 #ifndef SKARR_H
 #define SKARR_H
 
@@ -12,14 +18,21 @@ typedef struct {
     u8 data[];
 } skarr;
 
-void* skarr_create(void** dst, u32 size, u32 alloc);
-u32 skarr_size(void** arr);
-u32 skarr_capacity(void** arr);
-
-void* __skarr_push(void** arr, u32 typesize);
+void* skarr_create(void** dst, u32 size, u32 alloc);    // Creates a new array.
+u32 skarr_size(void** arr);                             // Returns the number of elements.
+u32 skarr_capacity(void** arr);                         // Returns the allocated bytes.
+void skarr_free(void** arr);                            // Frees the entire data structure.
+void skarr_reset(void** arr);                           // Removes all elements and frees allocated memory.
+void skarr_clear(void** arr);                           // Removes all elements keeps memory allocated and sets it all to 0.
+void skarr_erase(void** arr);                           // Removes all elements keeping memory untouched.
+void* __skarr_push(void** arr, u32 typesize);           // Makes room for a new element and returns a pointer to that memory.
+void* __skarr_pop(void** arr, u32 typesize);            // Removes the last element and return a pointer to that memory.
 
 #define _skarr_push(__skarr__) \
     ((typeof(*__skarr__))(__skarr_push((void*)__skarr__, sizeof(**__skarr__))))
-#define skarr_push(__skarr__, value) (*_skarr_push(__skarr__) = value)
+#define skarr_push(__skarr__, value) \
+    (*_skarr_push(__skarr__) = value)
+#define skarr_pop(__skarr__) \
+    ((typeof(**__skarr__))*((typeof(*__skarr__))__skarr_pop((void**)__skarr__, sizeof(**__skarr__))))
     
 #endif
